301bfd1d4e97c39893ee4c8d78e7d0ad6a4bae02
Benjamin Khong <benkhong27@gmail.com>
2026-06-28 16:47:46 -0700

    implemented fixed window algorithm

---
+++ limiter/fixed_window.go
@@ -0,0 +1,76 @@
+package limiter
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type FixedWindowLimiter struct {
+	client *redis.Client
+	limit  int
+	// time.Duration represents a length of time. We use it for ambiguity.
+	// Ex: NewFixedWindow(client, 100, 1*time.Minute) vs
+	//     NewFixedWindow(client, 100, 60)
+	window time.Duration
+}
+
+// Constructors are functions that conveniently create and return a struct.
+func NewFixedWindow(client *redis.Client, limit int, window time.Duration) *FixedWindowLimiter {
+	// Remember that '*' means that this type is a pointer while & gives the address of it.
+	// Without '*' in the return type, FixedWindowLimiter gets copied everytime NewFixedWindow is called.
+	return &FixedWindowLimiter{
+		client: client,
+		limit:  limit,
+		// window is the time when that window will expire
+		window: window,
+	}
+}
+
+// Fixed Window Counter: The time is split into windows and each have their own counter. After incrementing
+// the counter, check if the counter exceeds the limit, and if so, reject the request.
+// Also, the (fw *FixedWindowLimiter) part is called a reciever. It helps us access a struct's fields
+// (similar to self in Python).
+func (fw *FixedWindowLimiter) Allow(ctx context.Context, key string) (Result, error) {
+	// .Truncate() rounds the time to the nearest value you pass in it
+	windowStart := time.Now().Truncate(fw.window)
+	resetAt := windowStart.Add(fw.window)
+
+	// If we were to pass the key instead of a window key, we would be relying on Redis TTL.
+	// Not relying on TTL is important because it can lag. Also, .Unix() converts a time.Time
+	// value into a plain integer (the number of seconds since Jan 1st, 1970).
+	windowKey := fmt.Sprintf("%s:%d", key, windowStart.Unix())
+
+	redisClient := fw.client
+
+	// If INCR is called on a key that doesn't exist yet, it returns 1
+	count, err := redisClient.Incr(ctx, windowKey).Result()
+	if err != nil {
+		return Result{}, fmt.Errorf("error: could not increment redis counter: %w", err)
+	}
+
+	// If first request in that window, set an expiry
+	if count == 1 {
+		_, err = redisClient.Expire(ctx, windowKey, fw.window).Result()
+		if err != nil {
+			return Result{}, fmt.Errorf("error: could not set expiry: %w", err)
+		}
+	}
+
+	if count <= int64(fw.limit) {
+		return Result{
+			Allowed:   true,
+			Remaining: fw.limit - int(count),
+			ResetAt:   resetAt,
+		}, nil
+	} else {
+		return Result{
+			Allowed:   false,
+			Remaining: 0,
+			ResetAt:   resetAt,
+		}, nil
+	}
+
+}
