3771b676dd039216fcff2e712d5d8e31125da88b
Benjamin Khong <benkhong27@gmail.com>
2026-07-04 02:23:06 -0700

    implemented lua script to prevent redis race conditions

--- limiter/token_bucket.go
+++ limiter/token_bucket.go
@@ -3,7 +3,6 @@ package limiter
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -23,52 +22,62 @@ func NewTokenBucket(client *redis.Client, capacity int, refillRate float64) *Tok
 	}
 }
 
+var tokenBucketScript = `
+	local keys = KEYS[1]
+	local capacity = tonumber(ARGV[1])
+	local refill_rate = tonumber(ARGV[2])
+	local now = tonumber(ARGV[3])
+
+	local result = redis.call("HMGET", keys, "tokens", "lastRefill")
+	local tokens = result[1] -- Lua arrays start at 1, not 0 
+	local lastRefill = result[2]
+
+	if tokens == false then
+		tokens = capacity 
+	end 
+	if lastRefill == false then
+		lastRefill = now 
+	end
+
+	local elapsedTime = now - lastRefill
+	local tokensToAdd = elapsedTime * refill_rate
+
+	tokens = math.min(capacity, tokens+tokensToAdd)
+
+	local allowed = 0
+	if tokens >= 1 then 
+		tokens = tokens - 1
+		allowed = 1
+	else 
+		allowed = 0
+	end
+
+
+	local remaining = tokens
+	redis.call("HSET", keys, "tokens", tokens, "lastRefill", now)
+	redis.call("EXPIRE", keys, 3600)
+
+	return {allowed, remaining}
+`
+
 func (tb *TokenBucketLimiter) Allow(ctx context.Context, key string) (Result, error) {
-	redisClient := tb.client
-	// HGETALL returns all fields and values of the hash stored at key (map[string]string).
-	hGetAllResult, err := redisClient.HGetAll(ctx, key).Result()
+	// Eval executes a server-side Lua script with the embedded Redis Lua interpreter.
+	// The return value depends on the script that was executed.
+	result, err := tb.client.Eval(ctx, tokenBucketScript,
+		[]string{key},
+		tb.capacity, tb.refillRate, time.Now().Unix(),
+	).Result()
 	if err != nil {
-		return Result{}, fmt.Errorf("error: HGetAll() command not working: %w", err)
+		return Result{}, fmt.Errorf("token bucket lua script failed for key %s: %w", key, err)
 	}
 
-	// Default to a full bucket if this key has never been seen
-	tokens := float64(tb.capacity)
-	lastRefill := float64(time.Now().Unix())
+	vals := result.([]interface{}) // tells Go "this is a slice"
+	allowed := vals[0].(int64)     // Since Lua doesn't have bools either 1 or 0
+	remaining := vals[1].(int64)   // second value
 
-	// If the key already exists
-	if len(hGetAllResult) != 0 {
-		tokens, err = strconv.ParseFloat(hGetAllResult["tokens"], 64)
-		if err != nil {
-			return Result{}, fmt.Errorf("error: ParseFloat() cannot convert tokens to float64: %w", err)
-		}
-
-		lastRefill, err = strconv.ParseFloat(hGetAllResult["lastRefill"], 64)
-		if err != nil {
-			return Result{}, fmt.Errorf("error: ParseFloat() cannot convert lastRefill to float64: %w", err)
-		}
-
-		timeNow := time.Now().Unix()
-		elapsedTime := float64(timeNow) - lastRefill
-
-		tokensToAdd := elapsedTime * tb.refillRate
-		// Cap at capacity — bucket cannot overflow
-		tokens = min(float64(tb.capacity), tokens+tokensToAdd)
-	}
-
-	allowed := tokens >= 1
-	if allowed {
-		tokens--
-	}
-
-	_, err = redisClient.HSet(ctx, key, "tokens", tokens, "lastRefill", lastRefill).Result()
-	if err != nil {
-		return Result{}, fmt.Errorf("error: HSet() command not working: %w", err)
-	}
-
-	if allowed {
-		return Result{Allowed: true, Remaining: int(tokens), ResetAt: time.Time{}}, nil
-	} else {
-		return Result{Allowed: false, Remaining: 0, ResetAt: time.Time{}}, nil
-	}
-
+	return Result{
+		Allowed:   allowed == 1,
+		Remaining: int(remaining),
+		ResetAt:   time.Time{},
+	}, nil
 }
