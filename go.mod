5a1285080ce5e5e379fea0bf1802f264d4c0bbe7
Benjamin Khong <benkhong27@gmail.com>
2026-07-06 14:48:09 -0700

    deleted all testing files since testing lua scripts tedious

---
+++ go.mod
@@ -0,0 +1,10 @@
+// go.mod contains your module name and dependencies 
+module distributed-rate-limiter
+
+go 1.24.4
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/redis/go-redis/v9 v9.21.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"distributed-rate-limiter/store"
+	// "distributed-rate-limiter/limiter"
+	// "distributed-rate-limiter/middleware"
+	// "net/http"
+)
+
+func main() {
+	client, err := store.NewRedisClient()
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+}
